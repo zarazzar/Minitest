25a7bd7870ab825712654b05218cd3530b64f49d
AzzarRizky <azzar.arza18@gmail.com>
2022-10-10 17:56:26 +0700

    add minitest tengah

--- minitestAzzar-tengah/app/src/main/java/com/if3b/minitestazzar_tengah/MainActivity.java
+++ minitestAzzar-tengah/app/src/main/java/com/if3b/minitestazzar_tengah/MainActivity.java
@@ -2,13 +2,27 @@ package com.if3b.minitestazzar_tengah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnpindah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnpindah = findViewById(R.id.btn_pindah);
+
+        getSupportActionBar().setTitle("Layout Satu");
+        btnpindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ minitestAzzar-tengah/app/src/main/java/com/if3b/minitestazzar_tengah/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.if3b.minitestazzar_tengah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        getSupportActionBar().setTitle("Layout Dua");
+
+
+
+    }
+}
