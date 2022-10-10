42c7a5bad9e130a88aa2c1edee8c6e02bc2049c9
AzzarRizky <azzar.arza18@gmail.com>
2022-10-10 17:17:26 +0700

    creating minitest kiri

--- app/src/main/java/com/if3b/kiri/MainActivity.java
+++ app/src/main/java/com/if3b/kiri/MainActivity.java
@@ -3,12 +3,23 @@ package com.if3b.kiri;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnTekan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTekan = findViewById(R.id.btn_tekan);
+        btnTekan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "Apa Kabar Kamuuuu", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
